e8a8896f960961cc91517b5368791a7b75c076d0
Rohith Jacob Sebastian <rsebast2@umd.edu>
2019-07-18 22:32:29 -0400

    take 2

---
+++ official333am-web/src/app/containers/artists/artists.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'artists',
+  templateUrl: './artists.component.html',
+  styleUrls: ['./artists.component.css']
+})
+export class ArtistsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
