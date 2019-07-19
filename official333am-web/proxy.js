49bdfd74066348877b3e8a49990145995147134d
official333am <mgmt333am@gmail.com>
2019-07-19 03:16:45 -0400

    Update proxy.js

--- official333am-web/proxy.js
+++ official333am-web/proxy.js
@@ -1,7 +1,7 @@
 var HttpsProxyAgent = require('https-proxy-agent');
 var proxyConfig = [{
   context: '/api',
-  target: 'https://official333am.com',
+  target: 'https://official333am.github.io',
   secure: false
 }];
 
