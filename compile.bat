45f6a49d297048b2e51970d4b32256ac8312bd94
official333am <mgmt333am@gmail.com>
2019-07-20 11:30:43 -0400

    update - Sat 07-20-2019

---
+++ compile.bat
@@ -0,0 +1,3 @@
+set datestr=%date%
+set result=%datestr:/=-%
+ng build && copy CNAME "/docs" && git add . && git commit -m "update - %result%"
--- official333am-web/compile.bat
+++
@@ -1,3 +0,0 @@
-set datestr=%date%
-set result=%datestr:/=-%
-ng build && copy CNAME "../docs" && git add "../" && git --git-dir="../.git/" --work-tree="../" commit -m "update - %result%"
