b0b1e99972cc31a8fb1dc6b46e3065090a8654b1
official333am <mgmt333am@gmail.com>
2019-07-20 13:27:04 -0400

    update - Sat 07/20/2019 13:26:33.08

---
+++ src/app/components/artist-cards/artist-cards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'artist-cards',
+  templateUrl: './artist-cards.component.html',
+  styleUrls: ['./artist-cards.component.css']
+})
+export class ArtistCardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
