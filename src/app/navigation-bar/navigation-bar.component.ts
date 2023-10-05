c7807ec19fa54f47a78e19a7fc24d22dcaae3fd8
Rohith Sebastian <rohithsebastian@Rohiths-MacBook-Pro.local>
2023-10-05 16:31:01 -0400

    update - %date% %time%

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var $: any;
 
 @Component({
   selector: 'app-navigation-bar',
   templateUrl: './navigation-bar.component.html',
   styleUrls: ['./navigation-bar.component.css']
 })
-export class NavigationBarComponent {
+export class NavigationBarComponent implements OnInit {
+
+  ngOnInit() {
+    // $(document).scroll(function () {
+    //   var $nav = $(".navbar");
+    //   $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    // });
+  }
 
 }
